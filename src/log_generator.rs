2e85c52dc57517bdec1292d8648961f3af093180
Jamie Melton <jasm1@st-andrews.ac.uk>
2025-07-26 00:42:51 +0100

    Initial Commit

---
+++ src/log_generator.rs
@@ -0,0 +1,8 @@
+use std::io;
+use std::fs;
+
+// creates and populates the log file 
+pub fn generate_logs() -> io::Result {
+    // create the file
+    let mut log: fs::File = fs::Open
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod log_generator;
+mod log_analyser;
+
+fn main() {
+    
+}

0201c1df497434dcf369149cf9c993e5897e58bc
Jamie Melton <jasm1@st-andrews.ac.uk>
2025-07-26 01:00:33 +0100

    Completed log generator

--- src/log_generator.rs
+++ src/log_generator.rs
@@ -1,8 +1,33 @@
-use std::io;
-use std::fs;
+use std::io::{Result, BufWriter, Write};
+use std::fs::OpenOptions;
 
 // creates and populates the log file 
-pub fn generate_logs() -> io::Result {
+pub fn generate_log() -> Result<()> {
     // create the file
-    let mut log: fs::File = fs::Open
+    let log = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open("logs.txt")?;
+
+    // create buffer
+    let mut writer = BufWriter::new(log);
+
+    // add content
+    for _ in 1..=5000 {
+        writeln!(writer, "{}", get_entry())?;
+    }
+
+    Ok(())
+}
+
+fn get_entry() -> String {
+    match rand::random_range(1..=6) {
+        1 => "INFO: User logged in from 192.168.1.5",
+        2 => "WARNING: Disk Space low",
+        3 => "ERROR: Failed to open config file",
+        4 => "INFO: Job completed successfully",
+        5 => "ERROR: Timeout from 192.168.1.100",
+        6 => "INFO: User logged out",
+        _ => "ERROR: Unexpected behaviour occurred"
+    }.to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod log_generator;
 mod log_analyser;
 
-fn main() {
-    
+use std::io::Result;
+
+fn main() -> Result<()> {
+    // create the log file
+    log_generator::generate_log()?;
+
+    // analyse the log file
+
+    Ok(())
 }
